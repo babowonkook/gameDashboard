828fbe7e6b80c022e83995f78bfc299476a02030
babowonkook <babowonkook@gmail.com>
2023-11-27 18:23:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ dragula([
   document.getElementById("to-do"),
   document.getElementById("doing"),
   document.getElementById("done"),
-  document.getElementById("trash")
-]);
-removeOnSpill: false
-  .on("drag", function(el) {
+  document.getElementById("trash"),
+  document.getElementById("scheduled")
+]).on("drag", function(el) {
     el.className.replace("ex-moved", "");
   })
-  .on("drop", function(el) {
+  .on("drop", function(el, target, source, sibling) {
+    // el: 드래그하고 있는 요소
+    // target: el이 드래그 후 놓아진 리스트 요소
+    // sibling: 자리에 놓았을 때, 바로 그 다음 요소
+    // source: 원래 el이 있던 리스트 요소
     el.className += "ex-moved";
+    var childrenLength = target.children.length;
+    console.log(childrenLength);
+    if(el.classList.contains('task') && "scheduled" == target.id){
+      var idNum = childrenLength++;
+      document.getElementById("scheduled").innerHTML +=
+        "<li class='column doing-column'>" +
+        "<div class='column-header'>" +
+        "  <h4>"+idNum+"번 대기</h4>" +
+        "</div>" +
+        "<ul class='task-list' id='doing"+idNum+"'>" +
+        "</ul>" +
+        "</li>";
+
+      this.cancel;
+    }
+    
   })
   .on("over", function(el, container) {
     container.className += "ex-over";
@@ -20,14 +39,18 @@ removeOnSpill: false
   });
 
 /* Vanilla JS to add a new task */
-function addTask() {
+function addGuest() {
   /* Get task text from input */
-  var inputTask = document.getElementById("taskText").value;
+  var inputGuest = document.getElementById("guestText").value.trim();
+  if(inputGuest == ''){
+    alert("게스트를 입력해주세요.");
+    return false;
+  } 
   /* Add task to the 'To Do' column */
-  document.getElementById("to-do").innerHTML +=
-    "<li class='task'><p>" + inputTask + "</p></li>";
+  document.getElementById("done").innerHTML +=
+    "<li class='task'><p>" + inputGuest + "</p></li>";
   /* Clear task text from input after adding task */
-  document.getElementById("taskText").value = "";
+  document.getElementById("guestText").value = "";
 }
 
 /* Vanilla JS to delete tasks in 'Trash' column */
